3308e2dc5ecf8d7d4e6aadfbe6f676ca4e283cd5
Yannick Gloster <yannickgloster@gmail.com>
2022-11-15 18:42:42 +0000

    Added min max housing

--- user-interface/pages/index.js
+++ user-interface/pages/index.js
@@ -22,6 +22,34 @@ const queries = [
   }
   `,
   },
+  {
+    label: "Min Max Housing",
+    query: `PREFIX foaf:<http://xmlns.com/foaf/0.1/>
+    SELECT * WHERE
+    {
+    {
+    select ?house ?value ?area ?year 
+    where {
+        ?house foaf:Value ?value .
+        ?house foaf:Area ?area .
+        ?house foaf:Year ?year .
+    } order by desc (?value)
+    limit 1
+    }
+    UNION
+    {
+    select ?house ?value ?area ?year 
+    where {
+        ?house foaf:Value ?value .
+                filter(?value > 0)
+        ?house foaf:Area ?area .
+        ?house foaf:Year ?year .
+    } order by asc (?value)
+    limit 1
+    }
+    }
+  `,
+  },
   { label: "Is there a correlation between new house prices and crime?" },
   {
     label: "Is there a correlation between second hand house prices and crime?",
